027aed198bee37007824baf9d906b207d671c55f
dinnerTime <exmoure@gmail.com>
2018-05-20 21:41:58 -0600

    remove soft link for .vim pathing, no need for it

--- install.sh
+++ install.sh
@@ -5,16 +5,15 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # install neovim
 brew install neovim
-mkdir -p ~/.config/nvim
-ln -s ~/.config/nvim ~/.vim
-ln -s ~/.config/nvim/init.vim ~/.vimrc
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle && \
+curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# update zshrc with custom aliases
-`echo 'ssh-add ~/.ssh/id_rsa_github' >> ~/.zshrc`
+ update zshrc with custom aliases
 `echo 'export PROJ="$HOME/proj"' >> ~/.zshrc`
 `echo 'alias vim="nvim"' >> ~/.zshrc`
 `echo 'alias vi="nvim"' >> ~/.zshrc`
+`echo 'ssh-add ~/.ssh/id_rsa_github' >> ~/.zshrc`
 
 `source ~/.zshrc`
+
